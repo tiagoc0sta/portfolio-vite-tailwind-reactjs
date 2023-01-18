5ffed3f5beb3e539c2c848c994deee0beb85f43d
Tiago Costa <tiagodpv@gmail.com>
2023-01-17 21:14:39 -0500

    nav 1

--- src/components/Sidenav.jsx
+++ src/components/Sidenav.jsx
@@ -1,16 +1,21 @@
 import React, { useState } from 'react';
-import { AiOutlineMenu } from 'react-icons/ai';
+import { AiOutlineMenu, AiOutlineHome } from 'react-icons/ai';
 
 const Sidenav = () => {
   const [nav, setNav] = useState(false);
 
   const handleNav = () => {
     setNav(!nav);
+    console.log('state changed');
   };
 
   return (
     <div>
-      <AiOutlineMenu />
+      <AiOutlineMenu
+        onClick={handleNav}
+        className='absolute top-4 right-4 z-[99] md:hidden'
+      />
+      {nav ? <div></div> : <div></div>}
     </div>
   );
 };
