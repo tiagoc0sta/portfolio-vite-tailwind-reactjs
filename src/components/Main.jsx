75b02c9a8420e3909cefda90e93b00212f5978d1
Tiago Costa <tiagodpv@gmail.com>
2023-01-26 22:26:57 -0500

    add icons

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,4 +1,11 @@
 import React from 'react';
+import { TypeAnimation } from 'react-type-animation';
+import {
+  FaTwitter,
+  FaFacebookF,
+  FaInstagram,
+  FaLinkedinIn,
+} from 'react-icons/fa';
 
 const Main = () => {
   return (
@@ -8,9 +15,35 @@ const Main = () => {
         src='https://images.unsplash.com/photo-1543357480-c60d40007a3f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80'
         alt=''
       />
-      <div className='w-full h-screen absolute top-0 left-0 bg-white/50'></div>
-      <div>
-        <h1>I'm John Adams</h1>
+      <div className='w-full h-screen absolute top-0 left-0 bg-white/50'>
+        <div className='max-w-[700px] m-auto h-full flex flex-col justify-center lg:items-start items-center'>
+          <h1 className='sm:text-5xl text-4xl front-bold text-gray-800'>
+            I'm John Adams
+          </h1>
+          <h2 className='flex sm:text-3xl text-2xl pt-4 text-gray-800'>
+            I'm a
+            <TypeAnimation
+              sequence={[
+                'Developer', // Types 'One'
+                2000, // Waits 1s
+                'Coder', // Deletes 'One' and types 'Two'
+                2000, // Waits 2s
+                'Tech Enthusiast', // Deletes 'One' and types 'Two'
+                2000, // Waits 2s
+              ]}
+              wrapper='div'
+              cursor={true}
+              repeat={Infinity}
+              style={{ fontSize: '1em', paddingLeft: '5px' }}
+            />
+          </h2>
+          <div className='flex justify-between pt-6 max-w-[200px] w-full'>
+            <FaTwitter className='cursor-pointer' size={20} />
+            <FaFacebookF className='cursor-pointer' size={20} />
+            <FaInstagram className='cursor-pointer' size={20} />
+            <FaLinkedinIn className='cursor-pointer' size={20} />
+          </div>
+        </div>
       </div>
     </div>
   );
